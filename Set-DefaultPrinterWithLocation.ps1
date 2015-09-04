154a8a3924f81e4f7819fd134d12117d4df1ea70
webtroter <webtroter.alexis@gmail.com>
2015-09-04 09:56:39 -0400

    1.2
    - Useless comments
    - Debug messages
    + Helpful comments

--- Set-DefaultPrinterWithLocation.ps1
+++ Set-DefaultPrinterWithLocation.ps1
@@ -1,28 +1,19 @@
-﻿<#Import Modules#>
-[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") > $null
-
-
-#Setting Variables
+﻿#Setting Variables
 
 $clientPC = Get-ADComputer -Identity $env:CLIENTNAME -Properties Location
+
+#Ici, si location est vide, exit
 if ($clientpc.Location -eq $null){ exit }
+
 $clientPrinter = Get-Printer -computerName $clientpc.Name | Where-Object {$_.Name -contains $clientPC.Location}
 $clientPrinterPath = "\\" + $clientPrinter.ComputerName + "\" + $clientPrinter.Name
 Add-Printer -ConnectionName $clientPrinterPath
 
 
 $WMIPrinter = Get-WmiObject -Class Win32_Printer | where {$_.Name -like $clientPrinterPath }
-#$_.Name -match $clientPrinter.Name -and 
 
-#(Get-WmiObject -ComputerName . -Class Win32_Printer -Filter "Name='HP LaserJet 5Si'").SetDefaultPrinter()
+#[System.Windows.Forms.MessageBox]::Show("Settings " + $WMIPrinter.DeviceID + " as default Printer")
 
-[System.Windows.Forms.MessageBox]::Show("Settings " + $WMIPrinter.DeviceID + " as default Printer")
 $WMIPrinter.setdefaultprinter()
 
 Exit
-
-<#
-$clientPrinterPath = "\\" + $clientPrinter.ComputerName + "\" + $clientPrinter.Name
-(New-Object -ComObject WScript.Network).AddWindowsPrinterConnection($clientPrinterPath)
-
-#>
